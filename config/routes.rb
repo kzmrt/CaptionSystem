838a0a877b8b35aeab66ca4f3c8b4ad84d277c25
kzmrt <info.murata.kazuhiko@gmail.com>
2018-09-11 15:38:55 +0900

    Sidekiqの閲覧制限追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  mount Sidekiq::Web, at: '/sidekiq'
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
